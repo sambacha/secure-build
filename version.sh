4a454c4ff04b1fecb6bb9fcc1db962b4156c4c41
sam bacha <sam@manifoldfinance.com>
2021-12-02 09:13:39 -0800

    init

---
+++ version.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+git_hash='$Format:%h$'
+
+if test "${git_hash}" != '$''Format:%h$'; then
+  echo "${git_hash}"
+  exit 0
+fi
+
+###############################################################################
+
+## METHOD 2: The source tree is a git repository.
+
+case $# in
+  0) ;;
+  1) if test ! -d "$1"; then
+       echo "error"
+       exit 1
+     fi
+     cd "$1"
+     if test $? -eq 1; then
+       echo "error"
+       exit 1
+     fi;;
+  *) echo "error"
+     exit 1;;
+esac
+
+git_hash=`git show -s --format=%h . 2>/dev/null`
+if test $? -eq 0; then
+  echo "${git_hash}"
+  exit 0
+fi
+
+###############################################################################
+
+## METHOD 3: Give up.
+
+echo "unknown"
+exit 0
